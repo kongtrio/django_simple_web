3509ec391fd92c9f6a40e6a9a266aca4fab59465
yangjb <kongtrio@sina.com>
2016-05-20 17:30:30 +0800

    add english translate

--- english/views.py
+++ english/views.py
@@ -25,12 +25,10 @@ def translate_word(request, word):
     if word is not None:
         word_list = Word.objects.filter(en_word=word)
         if get_word is None or len(word_list) == 0:
-            print 'not fount word:' + word
             translate_util = translate_en_word.TranslateUtil()
             result = translate_util.tranlate_word(word)
             if len(result) == 0:
                 not_foutn = True
-                print u'查询不到单词'
             else:
                 db_word = Word(en_word=word, cn_word=result[0], en_spell=result[1], sentence=result[2],
                                more_word=result[3],
